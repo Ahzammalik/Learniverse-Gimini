2721257ca31694e9532dd7ceb723a0b0a9ee3c66
AdProfitX <103428386+Ahzammalik@users.noreply.github.com>
2025-08-07 20:50:32 +0500

    Create ui.js

---
+++ js/ui.js
@@ -0,0 +1,3 @@
+// In js/ui.js -> renderHome()
+// Add this line inside the <div class="home-grid">
+<a href="#/poems" class="module-card" style="border-color: #E91E63; color: #E91E63;"><div class="icon">📜</div><h2>Kids Poems</h2></a>
